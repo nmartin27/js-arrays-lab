b774693da0a050a54be54e8d7594e472f51437e9
nmartin <nate.martin27@gmail.com>
2024-10-21 10:20:38 -0400

    Exercise 13

--- app.js
+++ app.js
@@ -196,7 +196,7 @@ for (const num of nums) {
     if (num % 5 === 0) {
         buzz.push(num)
     }
-    if (num % 3 === 0 && num % 5 === 0) {
+if (num % 3 === 0 && num % 5 === 0) {
         fizzbuzz.push(num)
     } 
 }
@@ -206,3 +206,25 @@ console.log('  fizz:', fizz);
 console.log('  buzz:', buzz);
 console.log('  fizzbuzz:', fizzbuzz);
 
+/*
+Exercise 13: Retrieve the Last Array
+
+1) Assign the last nested array in the `numArrays` below to a variable named
+   `numList`. As you do this, also fulfill these goals:
+
+   - Assume you don't know how many nested arrays `numArrays` contains.
+   - Do not alter the original `numArrays` array.
+
+Complete Exercise 13 in the space below:
+*/
+
+const numArrays = [
+	[100, 5, 23],
+	[15, 21, 72, 9],
+	[45, 66],
+	[7, 81, 90]
+];
+
+const numList = numArrays[numArrays.length - 1]
+
+console.log('Exercise 13 result:', numList);
