aefe0fc3553da0cb19c29f25fda8b0ed7662696a
nmartin <nate.martin27@gmail.com>
2024-10-21 08:45:56 -0400

    Exercise 4

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ Note: 'pizza' should be the first item in the array, followed by 'cheeseburger'.
 Complete Exercise 2 in the space below:
 */
  const foods = [];  
- foods.push('cheese Pizza', 'cheesburger');
+ foods.push('Pizza', 'cheesburger');
 
 console.log('Exercise 2 result:', foods);
 
@@ -37,3 +37,19 @@ foods.unshift('taco')
 
 
 console.log('Exercise 3 result:', foods);
+
+
+/*
+Exercise 4: Access an array element
+
+1) Retrieve the 'pizza' string from the array based on its position (index) in
+   the array.  
+
+2) Assign it to a variable called `favFood`.
+
+Complete Exercise 4 in the space below:
+*/
+
+const favFood = foods[1]
+
+console.log('Exercise 4 result:', favFood);
